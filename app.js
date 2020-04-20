69b2c98e202685e95a24dfa44c78ce1d53560e5c
Tanasescu Octavian <tanasescu.octaviann@gmail.com>
2020-04-20 22:13:56 +0300

    Added visuals

--- app.js
+++ app.js
@@ -1,85 +1,86 @@
-function game() {
-    let rounds = 0;
-    let playerScore = 0;
-    let computerScore = 0;
+let userScore = 0;
+let compScore = 0;
+let buttons = document.querySelectorAll('button');
+game();
+function game(){
+    buttons.forEach(button => button.addEventListener('click', displayRound));
+}
+function displayRound(e){
+    let compChoice = computerPlay();
+    let signComp = document.querySelector('#ChoiceComp');
+    let signUser = document.querySelector('#ChoiceUser');
+    signUser.textContent = 'You chose: ' + e.target.id;
+    signComp.textContent = 'The computer chose: ' + compChoice;
 
-    while (rounds < 5) {
-        playRound(playerPlay(), computerPlay());
-        rounds++;
+    console.log(playRound(e.target.id, compChoice));
+    const score = document.querySelector('#currScore');
+    if(userScore === 5 || compScore === 5){
+        buttons.forEach(button => button.removeEventListener('click', displayRound));
+        let h3 = document.querySelector('h3');
+        h3.textContent = 'Final Score';
+        if(userScore > compScore){
+            score.textContent = `Computer:  ${compScore}  You:  ${userScore}    YOU WIN!`;
+        }
+        else{
+            score.textContent = `Computer:   ${compScore}  You:  ${userScore}     COMPUTER WINS!`;
+        }
+        return;
     }
-
-    score();
-
-    function playerPlay() {
-        let playerSelection = prompt(' rock | paper | scissors ');
-        if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
-            console.log(`Your choice is ${playerSelection}.`)
-            return playerSelection;
+    score.textContent = 'Computer: ' + compScore + ' You: ' + userScore;
+}
+function computerPlay(){
+    let rand = Math.random();
+    if(rand <= 1/3){
+        return "Rock";
+    }
+    else if(rand <= 2/3){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.match(/rock/i)){
+        if(computerSelection == "Rock"){
+            return "Draw!";
+        }
+        else if(computerSelection == "Paper"){
+            compScore++;
+            return "You lose! Paper beats Rock.";
+        }
+        else if(computerSelection == "Scissors"){
+            userScore++;
+            return "You win! Rock beats Scissors.";
         }
     }
-
-    function computerPlay() {
-        const options = ['rock', 'paper', 'scissors'];
-        computerSelection = options[Math.floor(Math.random() * options.length)];
-        return computerSelection;
-    }
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection == 'rock' && computerSelection == 'scissors') {
-            playerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You won, because ${playerSelection} beats ${computerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return playerScore;
-        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-            playerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You won, because ${playerSelection} beats ${computerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return playerScore;
-        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-            playerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You won, because ${playerSelection} beats ${computerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return playerScore;
-        } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
-            computerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You lost, because ${computerSelection} beats ${playerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return computerScore;
-        } else if (computerSelection == 'paper' && playerSelection == 'rock') {
-            computerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You lost, because ${computerSelection} beats ${playerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return computerScore;
-        } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
-            computerScore++;
-            console.log(`Computer's choice is ${computerSelection}`);
-            console.log(`You lost, because ${computerSelection} beats ${playerSelection}`);
-            console.log(`Your score is ${playerScore} | Computer's score is ${computerScore}`);
-            return computerScore;
+    else if(playerSelection.match(/paper/i)){
+        if(computerSelection == "Paper"){
+            return "Draw!";
         }
-        else {
-            playerScore = playerScore;
-            console.log(`Computer's choice is ${computerSelection}.`)
-            console.log(`Tie, because both chose ${playerSelection}`);
-            console.log(`Scores remain the same: ${playerScore} | ${computerScore}`);
+        else if(computerSelection == "Rock"){
+            userScore++;
+            return "You win! Paper beats Rock.";
+        }
+        else if(computerSelection == "Scissors"){
+            compScore++;
+            return "You lose! Scissors beat Paper.";
         }
     }
-
-    function score() {
-        let result;
-        console.log(`FINAL SCORE: ${playerScore} | ${computerScore}`);
-        if (playerScore > computerScore) {
-            result = console.log('YOU WON!');
-        } else if (playerScore < computerScore) {
-            result = console.log('YOU LOST!');
-        } else {
-            result = console.log('TIE!');
+    else if(playerSelection.match(/scissors/i)){
+        if(computerSelection == "Scissors"){
+            return "Draw!";
         }
-        return result;
+        else if(computerSelection == "Paper"){
+            userScore++;
+            return "You win! Scissors beat Paper.";
+        }
+        else if(computerSelection == "Rock"){
+            compScore++;
+            return "You lose! Rock beats Scissors.";
+        }
+    }
+    else{
+        return "The Input was invalid.";
     }
 }
